e71c54be4929e81e72b140f25bb2aea92643e113
Restyled.io <commits@restyled.io>
2022-12-17 01:17:22 +0000

    Restyled by prettier

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,11 +1,11 @@
-const sequelize = require('../config/connection');
-const User = require('../models/User');
+const sequelize = require("../config/connection");
+const User = require("../models/User");
 
 const userData = [
-  {username : 'test', email : 'test@test.com', password : 'password12345'},
-  {username : 'test2', email : 'test2@test.com', password : 'password987654'},
+  { username: "test", email: "test@test.com", password: "password12345" },
+  { username: "test2", email: "test2@test.com", password: "password987654" },
 ];
 
-const seedUsers = () => User.bulkCreate(userData, {individualHooks : true});
+const seedUsers = () => User.bulkCreate(userData, { individualHooks: true });
 
 module.exports = seedUsers;
