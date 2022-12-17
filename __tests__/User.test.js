4c5c8a9866d8b57ce2cb6ec973d7916bf89a83b1
Daniel Flores D <daniel.flor3s.d@gmail.com>
2022-12-16 18:16:00 -0700

    Project setup and user model definition

---
+++ __tests__/User.test.js
@@ -0,0 +1,14 @@
+
+var User = require('../models/User');
+
+describe('User', () => {
+    it('should create a new user', () => {
+        const user = new User();
+        expect(user).toBeDefined();
+        expect(user).toBeInstanceOf(User);
+        expect(user).toHaveProperty('id');
+        expect(user).toHaveProperty('username');
+        expect(user).toHaveProperty('email');
+        expect(user).toHaveProperty('password');
+    });
+});
