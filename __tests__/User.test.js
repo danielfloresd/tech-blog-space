4b9ad87582d5e2382bb55be5ab12f18d458a0c19
Restyled.io <commits@restyled.io>
2022-12-18 20:16:06 +0000

    Restyled by prettier

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -1,13 +1,13 @@
-var User = require('../models/User');
+var User = require("../models/User");
 
-describe('User', () => {
-  it('should create a new user', () => {
+describe("User", () => {
+  it("should create a new user", () => {
     const user = new User();
     expect(user).toBeDefined();
     expect(user).toBeInstanceOf(User);
-    expect(user).toHaveProperty('id');
-    expect(user).toHaveProperty('name');
-    expect(user).toHaveProperty('email');
-    expect(user).toHaveProperty('password');
+    expect(user).toHaveProperty("id");
+    expect(user).toHaveProperty("name");
+    expect(user).toHaveProperty("email");
+    expect(user).toHaveProperty("password");
   });
 });
--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,18 @@ const Sequelize = require("sequelize");
 // Create connection to our database, pass in your MySQL information for
 // username and password
 const sequelize = process.env.JAWSDB_URL
-                      ? new Sequelize(process.env.JAWSDB_URL)
-                      : new Sequelize(process.env.DB_NAME, process.env.DB_USER,
-                                      process.env.DB_PASSWORD, {
-                                        host : "localhost",
-                                        dialect : "mysql",
-                                        dialectOptions : {
-                                          decimalNumbers : true,
-                                        },
-                                      });
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 module.exports = sequelize;
