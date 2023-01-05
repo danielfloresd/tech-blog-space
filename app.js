d1c0ef3d00ec7ff9238c2f6255945aaddb5fa363
Daniel Flores D <daniel.flor3s.d@gmail.com>
2023-01-05 10:35:37 -0700

    Add maxAge to cookie

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const path = require('path');
-const express = require('express');
-const controllers = require('./controllers');
-const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
+const path = require("path");
+const express = require("express");
+const controllers = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const helpers = require("./utils/helpers");
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: "Super secret secret",
+  cookie: {
+    // maxAge sets the maximum age for the cookie to be valid. Here, the cookie (and session) will expire after one hour. The time should be given in milliseconds.
+    maxAge: 60 * 60 * 1000,
+    // httpOnly tells express-session to only store session cookies when the protocol being used to connect to the server is HTTP.
+    httpOnly: true,
+    // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
+    secure: false,
+    // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 const hbs = exphbs.create({ helpers });
@@ -24,10 +33,10 @@ const app = express();
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on routes
 app.use(controllers);
